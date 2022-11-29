0d3d14fe83bb051ae95cca4246812368dcc1e296
melody <b08504045@ntu.edu.tw>
2022-11-29 12:45:29 +0800

    QQ

---
+++ hack2/backend/src/routes/info.js
@@ -0,0 +1,73 @@
+// * ////////////////////////////////////////////////////////////////////////
+// *
+// * FileName     [ info.js ]
+// * PackageName  [ server ]
+// * Synopsis     [ Get restaurant info from database ]
+// * Author       [ Chin-Yi Cheng ]
+// * Copyright    [ 2022 11 ]
+// *
+// * ////////////////////////////////////////////////////////////////////////
+
+import mongoose from 'mongoose'
+import Info from '../models/info'
+
+exports.GetSearch = async (req, res) => {
+    /*******    NOTE: DO NOT MODIFY   *******/
+    const priceFilter = req.query.priceFilter
+    const mealFilter  = req.query.mealFilter
+    const typeFilter  = req.query.typeFilter
+    const sortBy      = req.query.sortBy
+    /****************************************/
+    // const db = mongoose.connect.db()
+    // NOTE Hint: 
+    // use `db.collection.find({condition}).exec(err, data) {...}`
+    // When success, 
+    //   do `res.status(200).send({ message: 'success', contents: ... })`
+    // When fail,
+    //   do `res.status(403).send({ message: 'error', contents: ... })` 
+    const condition = {
+        price:priceFilter,
+        meal:mealFilter,
+        type:typeFilter
+
+    }
+    // console.log(db.collection)
+    try{
+        const test = Info.find()
+        console.log(test)
+        // Info.find({})
+        // console.log(db.collection)
+    // var data = db.collection.find({condition}).exec(err, data) 
+    res.status(200).send({ message: 'success', contents: data})
+    } catch(e){
+        res.status(403).send({ message: 'error', contents: 'restaurent found error' })
+    }
+
+        
+    
+
+    // TODO Part I-3-a: find the information to all restaurants
+    
+    // TODO Part II-2-a: revise the route so that the result is filtered with priceFilter, mealFilter and typeFilter
+    // TODO Part II-2-b: revise the route so that the result is sorted by sortBy
+}
+
+exports.GetInfo = async (req, res) => {
+    /*******    NOTE: DO NOT MODIFY   *******/
+    const id = req.query.id
+    /****************************************/
+
+    // NOTE USE THE FOLLOWING FORMAT. Send type should be 
+    // if success:
+    // {
+    //    message: 'success'
+    //    contents: the data to be sent. Hint: A dictionary of the restaruant's information.
+    // }
+    // else:
+    // {
+    //    message: 'error'
+    //    contents: []
+    // }
+
+    // TODO Part III-2: find the information to the restaurant with the id that the user requests
+}
